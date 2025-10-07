ccb7bbbf57dd21a5324c788114a852acf3416d18
Leanitech <akarmakar846@gmail.com>
2025-10-07 23:22:12 +0530

    refactor(auth): restructure auth client configuration and type definitions
    Refactors auth client configuration and type definitions
    
    Restructures the auth client setup by separating plugins into a
    dedicated array and introducing a configuration object to improve
    readability and maintainability. Updates type definitions to align
    with the new structure, ensuring better type safety and flexibility.

--- lib/auth/auth-client.ts
+++ lib/auth/auth-client.ts
@@ -4,17 +4,28 @@ import {
   usernameClient,
 } from 'better-auth/client/plugins';
 import { createAuthClient } from 'better-auth/react';
+import type { BetterAuthClientPlugin } from 'better-auth/types';
+
+import { ClientConfigs } from '@/types/better-auth-types';
 import { auth } from './auth';
 
-export const authClient = createAuthClient({
-  // baseURL: 'http://localhost:3000', // The base URL of your auth server
-  plugins: [
-    inferAdditionalFields<typeof auth>(),
-    usernameClient(),
-    twoFactorClient({
-      onTwoFactorRedirect: () => {
-        window.location.href = '/two-factor';
-      },
-    }),
-  ],
-});
+const clientPlugins = [
+  inferAdditionalFields<typeof auth>(),
+  usernameClient(),
+  twoFactorClient({
+    onTwoFactorRedirect: () => {
+      window.location.href = '/two-factor';
+    },
+  }),
+] satisfies BetterAuthClientPlugin[];
+
+const authClientConfigs = {
+  // baseURL: 'http://localhost:3000',
+  // basePath: '/auth',
+  plugins: clientPlugins,
+  // disableDefaultFetchPlugins: false,
+  // fetchOptions: {},
+  // $InferAuth: {}
+} satisfies ClientConfigs;
+
+export const authClient = createAuthClient(authClientConfigs);
--- types/better-auth-types.ts
+++ types/better-auth-types.ts
@@ -1,9 +1,20 @@
+import { BetterAuthClientPlugin, BetterAuthOptions } from 'better-auth';
+import { BetterFetchOption } from 'better-auth/react';
+
 import { auth } from '@/lib/auth/auth';
 import { authClient } from '@/lib/auth/auth-client';
-
-// import { InferUser,Models,User } from "better-auth/types";
 
 export type Account = Awaited<
   ReturnType<typeof auth.api.listUserAccounts>
 >[number];
+
 export type UserWithAddlInfo = (typeof authClient.$Infer.Session)['user'];
+
+export type ClientConfigs = {
+  fetchOptions?: BetterFetchOption;
+  plugins?: BetterAuthClientPlugin[];
+  baseURL?: string;
+  basePath?: string;
+  disableDefaultFetchPlugins?: boolean;
+  $InferAuth?: BetterAuthOptions;
+};
